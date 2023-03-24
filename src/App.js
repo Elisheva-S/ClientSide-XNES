22a4e304f922b0cbeb93fe6e898d5c9c1ed7b6e2
Elisheva-S <105245801+Elisheva-S@users.noreply.github.com>
2023-03-24 05:16:39 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Route, Routes, NavLink } from "react-router-dom";
+import Departments from "./Department/Departments";
+import Details from "./Details/Details";
+import Products from "./Product/Products";
+import EditDep from "./EditDep/EditDep";
+import EditPro from "./EditPro/EditPro";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div align="center">
+      <nav className="navbar navbar-expand-sm bg-light navbar-dark">
+        <ul className="navbar-nav">
+          <li className="nav-iten m-1">
+            <NavLink
+              className="btn btn-dark btn-outline-primary"
+              to="Department/Departments"
+            >
+              Departments
+            </NavLink>
+            <NavLink
+              className="btn btn-dark btn-outline-primary"
+              to="Product/Products"
+            >
+              Products
+            </NavLink>
+            <NavLink
+              className="btn btn-light btn-outline-primary"
+              to="Details/Details"
+            >
+              Details
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="Department/Departments" element={<Departments />} />
+        <Route path="Product/Products" element={<Products />} />
+        <Route path="Details/Details" element={<Details />} />
+        <Route path="EditDep/EditDep" element={<EditDep />} loader />
+        <Route path="EditPro/EditPro" element={<EditPro />} loader />
+      </Routes>
     </div>
   );
 }
